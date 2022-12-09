419ed10c9d2d5570d38e5e7fe5c1cb932fe07a56
Hgs Infotech <hgsinfotech@gmail.com>
2022-12-09 17:53:59 +0530

    Adding files to git

---
+++ wp-content/plugins/embed-form/jotform-wp-embed.js
@@ -0,0 +1,62 @@
+/*
+    Plugin Name: Jotform Embed Forms
+    Plugin URI: https://www.jotform.com/labs/
+    Description:
+    Version: 1.2.6
+    Author: Jotform.com
+    Author URI: https://www.jotform.com
+    License: GNU General Public License v3
+*/
+
+(function() {
+    tinymce.create('tinymce.plugins.JotFormWPEmbed', {
+        init : function(ed, url) {
+            ed.addButton('JotFormWPEmbed', {
+                title : 'Jotform',
+                image : url+'/images/jotform.png',
+                onclick : function(event) {
+                    function openWizard() {
+                        window.jotformFormPicker.openWizard(function(data){
+                            var value;
+                            var isValid;
+                            if(typeof data !== 'string'){
+                                isValid = data && data.title && data.id && data.id !== "Please enter your form id";
+                                value = '[jotform id="' + data.id + '" title="'+data.title+'"]';
+                            } else{
+                                isValid = data.length > 0 && data !== "Please enter your form id";
+                                value = '[jotform id="' + data + '"]';
+                            }
+                            if (isValid) {
+                                tinyMCE.activeEditor.execCommand('mceInsertContent', 0, value);
+                            }
+                        });
+                    }
+                    if(window.jotformFormPicker) {
+                        event.preventDefault();
+                        openWizard();
+                    }
+                    else {
+                        jQuery.getScript("//js.jotform.com/JotFormFormPicker.js", function(data, textStatus, jqxhr) {
+                            event.preventDefault();
+                            window.jotformFormPicker = new JotFormFormPicker();
+                            openWizard();
+                        });
+                    }
+                }
+            });
+        },
+        createControl : function(n, cm) {
+            return null;
+        },
+        getInfo : function() {
+            return {
+                longname : "Jotform Embed Forms",
+                author : 'Jotform.com',
+                authorurl : 'https://www.jotform.com',
+                infourl : 'https://www.jotform.com/labs/',
+                version : "1.2.5"
+            };
+        }
+    });
+    tinymce.PluginManager.add('JotFormWPEmbed', tinymce.plugins.JotFormWPEmbed);
+})();
